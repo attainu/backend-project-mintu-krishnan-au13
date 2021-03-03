bf5156e7db917423dd3fdd32037e7fa84b247239
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-03-03 12:50:36 +0530

    added development and prod mode in config.env

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+
 const app = express();
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 const travelRouter = require('./routes/travelRouters');
 const userRouter = require('./routes/userRoutes');
 
-app.use(morgan('dev'));
 app.use(express.json());
 
 app.use('/api/v1/travels', travelRouter);
--- server.js
+++ server.js
@@ -1,6 +1,9 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
