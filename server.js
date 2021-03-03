cd10a1c753230ed88ada716dcb7863e893faff44
dimple-shah-au13 <dimps287980@gmail.com>
2021-03-04 00:08:31 +0530

    Added Schema and custom Data

--- server.js
+++ server.js
@@ -18,6 +18,37 @@ mongoose
     console.log('DB connected');
   });
 
+const travelSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A travel must have a name'],
+    unique: [true, ' A travel name must be unique'],
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A travel must have a price'],
+  },
+});
+const Travel = mongoose.model('travel', travelSchema);
+
+const testTravel = new Travel({
+  name: 'the sea hiker',
+  price: 497,
+});
+
+testTravel
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('❌ error ❌', err);
+  });
+
 const app = require('./app');
 
 const PORT = process.env.PORT || 3001;
