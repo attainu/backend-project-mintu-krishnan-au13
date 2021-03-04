92edc33535437e4013929295b6276d6b51a92217
dimple-shah-au13 <dimps287980@gmail.com>
2021-03-04 16:54:06 +0530

    updated all CRUD operations in Travel

--- controller/travelController.js
+++ controller/travelController.js
@@ -1,14 +1,38 @@
 const Travel = require('./../models/travelModel');
 
-exports.getAllTravels = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
+exports.getAllTravels = async (req, res) => {
+  try {
+    const travels = await Travel.find();
+    res.status(200).json({
+      status: 'success',
+      results: travels.length,
+      data: {
+        travels: travels,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
 };
-exports.getTravel = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-  });
+exports.getTravel = async (req, res) => {
+  try {
+    const travels = await Travel.findById(req.params.id);
+    res.status(200).json({
+      status: 'Success ✅',
+      results: travels.length,
+      data: {
+        travels: travels,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Fail Invalid Id ❌',
+      message: err,
+    });
+  }
 };
 exports.createTravel = async (req, res) => {
   try {
@@ -27,17 +51,40 @@ exports.createTravel = async (req, res) => {
   }
 };
 
-exports.updateTravel = (req, res) => {
-  res.status(200).json({
-    status: 'success',
-    data: {
-      travels: '<Updated tour content>',
-    },
-  });
+exports.updateTravel = async (req, res) => {
+  try {
+    const travels = await Travel.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'Success ✅',
+      results: travels.length,
+      data: {
+        travels: travels,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Fail Invalid Id ❌',
+      message: err,
+    });
+  }
 };
-exports.deleteTravel = (req, res) => {
-  res.status(204).json({
-    status: 'success',
-    data: null,
-  });
+exports.deleteTravel = async (req, res) => {
+  try {
+    const travels = await Travel.findByIdAndDelete(req.params.id);
+    res.status(200).json({
+      status: 'Success ✅',
+      results: travels.length,
+      data: {
+        travels: travels,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'Fail Invalid Id ❌',
+      message: err,
+    });
+  }
 };
