839ff802b181ab0d58b9d68bcea0851a3975469e
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-03-06 16:01:07 +0530

    Structured Api Features in utils folder

--- controller/travelController.js
+++ controller/travelController.js
@@ -1,4 +1,5 @@
 const Travel = require('./../models/travelModel');
+const APIfeatures = require('./../utils/apiFeatures');
 
 exports.aliasTopTravels = (req, res, next) => {
   req.query.limit = 5;
@@ -6,55 +7,6 @@ exports.aliasTopTravels = (req, res, next) => {
   req.query.fields = 'name,price,ratingsAverage,summary';
   next();
 };
-
-class APIfeatures {
-  constructor(query, queryString) {
-    this.query = query;
-    this.queryString = queryString;
-  }
-
-  filter() {
-    const queryObj = { ...this.queryString };
-    const excludeFields = ['page', 'sort', 'limit', 'fields'];
-    excludeFields.forEach((el) => delete queryObj[el]);
-
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    this.query.find(JSON.parse(queryStr));
-    return this;
-  }
-  sort() {
-    if (this.queryString.sort) {
-      const sortBy = this.queryString.sort.split(',').join(' ');
-      this.query.sort(sortBy);
-    } else {
-      this.query.sort('-createdAt');
-    }
-    return this;
-  }
-  limitFields() {
-    if (this.queryString.fields) {
-      const fields = this.queryString.fields.split(',').join(' ');
-      this.query.select(fields);
-    } else {
-      this.query.select('-__v');
-    }
-    return this;
-  }
-  paginate() {
-    const page = this.queryString.page * 1 || 1;
-    const limit = this.queryString.limit * 1 || 100;
-    const skip = (page - 1) * limit;
-
-    this.query.skip(skip).limit(limit);
-
-    // if (this.queryString.page) {
-    //   const numTravels = await this.query.countDocuments();
-    //   if (skip > numTravels) throw new Error('This page does not exists');
-    // }
-    return this;
-  }
-}
 
 exports.getAllTravels = async (req, res) => {
   try {
