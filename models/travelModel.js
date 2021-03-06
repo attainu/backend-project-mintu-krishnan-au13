f81ef8c8e9026a492b7fe5728105a229f687b248
dimple-shah-au13 <dimps287980@gmail.com>
2021-03-06 19:08:05 +0530

    Created Virtual properties in models

--- models/travelModel.js
+++ models/travelModel.js
@@ -1,58 +1,69 @@
 const mongoose = require('mongoose');
 const { deleteTravel } = require('../controller/travelController');
 
-const travelSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A travel must have a name'],
-    unique: true,
+const travelSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A travel must have a name'],
+      unique: true,
+    },
+    duration: {
+      type: Number,
+      required: [true, 'A travel must have a duration'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A travel must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A travel must have a difficulty'],
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A travel must have a price'],
+    },
+    priceDiscount: Number,
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'A travel must have a description'],
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A tour must have a cover image'],
+    },
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false,
+    },
+    startDates: [Date],
   },
-  duration: {
-    type: Number,
-    required: [true, 'A travel must have a duration'],
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A travel must have a group size'],
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A travel must have a difficulty'],
-  },
-  ratingsAverage: {
-    type: Number,
-    default: 4.5,
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0,
-  },
-  price: {
-    type: Number,
-    required: [true, 'A travel must have a price'],
-  },
-  priceDiscount: Number,
-  summary: {
-    type: String,
-    trim: true,
-    required: [true, 'A travel must have a description'],
-  },
-  description: {
-    type: String,
-    trim: true,
-  },
-  imageCover: {
-    type: String,
-    required: [true, 'A tour must have a cover image'],
-  },
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false,
-  },
-  startDates: [Date],
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+travelSchema.virtual('durationWeeks').get(function () {
+  return Math.round((this.duration / 7) * 10) / 10;
 });
+
 const Travel = mongoose.model('travel', travelSchema);
 
 module.exports = Travel;
