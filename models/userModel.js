56d51be3486cff5f1f0ea87ed8ae1179b3503a0f
dimple-shah-au13 <dimps287980@gmail.com>
2021-03-07 20:38:55 +0530

    bcrypt password created in userModel

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -22,8 +23,20 @@ const userSchema = new mongoose.Schema({
   passwordConfirm: {
     type: String,
     required: [true, 'Please confirm your password'],
+    // this only works on create and save
+    validate: function (el) {
+      return el === this.password;
+    },
   },
 });
+
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 8);
+  this.passwordConfirm = undefined;
+  next();
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
