72883f4227cec7c51e185807aedbf93a794ebdd5
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-03-07 11:54:19 +0530

    implementing a global error handling middleware

--- app.js
+++ app.js
@@ -14,10 +14,20 @@ app.use(express.json());
 
 app.use('/api/v1/travels', travelRouter);
 app.use('/api/v1/users', userRouter);
-app.all('*', (req, res) => {
-  res.status(404).json({
-    status: 'fail',
-    message: `Can't find ${req.originalUrl} on this server`,
+
+app.all('*', (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.statusCode || 'error';
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
   });
 });
 
