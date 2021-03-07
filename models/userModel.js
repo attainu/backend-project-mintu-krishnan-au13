4e0f2e58161db3cf188f3e06b048f542fffcc6d6
dimple-shah-au13 <dimps287980@gmail.com>
2021-03-07 23:39:55 +0530

    JWT Token handling conditions

--- models/userModel.js
+++ models/userModel.js
@@ -19,6 +19,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: [true, 'Please prove a password'],
     minlength: 8,
+    select: false,
   },
   passwordConfirm: {
     type: String,
@@ -36,6 +37,13 @@ userSchema.pre('save', async function (next) {
   this.passwordConfirm = undefined;
   next();
 });
+
+userSchema.methods.correctPassword = async function (
+  candidatePassword,
+  userPassword
+) {
+  return await bcrypt.compare(candidatePassword, userPassword);
+};
 
 const User = mongoose.model('User', userSchema);
 
