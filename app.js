fbed146cbb0f7c505fd102cecf131901b4156594
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-03-07 11:38:10 +0530

    handling unhandled routes

--- app.js
+++ app.js
@@ -14,5 +14,11 @@ app.use(express.json());
 
 app.use('/api/v1/travels', travelRouter);
 app.use('/api/v1/users', userRouter);
+app.all('*', (req, res) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 
 module.exports = app;
