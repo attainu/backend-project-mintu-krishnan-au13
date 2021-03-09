2c7aef04210729a7a00798df32ec099c47eb1d44
dimple-shah-au13 <dimps287980@gmail.com>
2021-03-09 18:36:05 +0530

    created review model Schema

---
+++ models/reviewModel.js
@@ -0,0 +1,39 @@
+// review / rating / createdAt / ref to travel / ref to user
+const mongoose = require('mongoose');
+const Travel = require('./travelModel');
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, 'Review can not be empty!'],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    travel: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'Travel',
+      required: [true, 'Review must belong to a travel.'],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: 'User',
+      required: [true, 'Review must belong to a user'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
