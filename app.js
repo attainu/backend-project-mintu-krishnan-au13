b251cf2c1002840e7082c0f8606d3c14a88a6bc4
Mintu Krishnan S <mintu.krish999@hotmail.com>
2021-03-09 12:42:26 +0530

    Data sanitization using xss nosql injection

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
-
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/apiError');
 const globalErrorHandler = require('./controller/errorController');
@@ -13,7 +14,7 @@ const app = express();
 
 //GLOBAL MIDDLEWARES
 //set security HTTP headers
-app.use(helmet())
+app.use(helmet());
 
 //Development logging
 if (process.env.NODE_ENV === 'development') {
@@ -23,15 +24,23 @@ if (process.env.NODE_ENV === 'development') {
 const limiter = rateLimit({
   max: 100,
   windows: 60 * 60 * 1000,
-  message: 'Too many requests from this IP, please try again in an hour!'
+  message: 'Too many requests from this IP, please try again in an hour!',
 });
 app.use('/api', limiter);
 
-
 //Body parser, reading data from body into req.body
-app.use(express.json({
-  limit: '10kb'
-}));
+app.use(
+  express.json({
+    limit: '10kb',
+  })
+);
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xss());
+
+// Serving static files
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/v1/travels', travelRouter);
 app.use('/api/v1/users', userRouter);
