080552faee2b3748c213229b6d2f95dabbe71c85
dimple-shah-au13 <dimps287980@gmail.com>
2021-03-09 10:00:45 +0530

    sending JWT via cookies

--- controller/authController.js
+++ controller/authController.js
@@ -12,8 +12,20 @@ const signToken = (id) => {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
 };
+
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
@@ -32,15 +44,6 @@ exports.signup = catchAsync(async (req, res, next) => {
   // });
   const newUser = await User.create({ ...req.body });
   createSendToken(newUser, 201, res);
-  // const token = signToken(newUser._id);
-
-  // res.status(201).json({
-  //   status: 'success',
-  //   token,
-  //   data: {
-  //     user: newUser,
-  //   },
-  // });
 });
 
 exports.login = catchAsync(async (req, res, next) => {
@@ -62,11 +65,6 @@ exports.login = catchAsync(async (req, res, next) => {
 
   // 3.If everythink ok. send token to client
   createSendToken(user, 200, res);
-  // const token = signToken(user._id);
-  // res.status(200).json({
-  //   status: 'success',
-  //   token,
-  // });
 });
 
 exports.protect = catchAsync(async (req, res, next) => {
